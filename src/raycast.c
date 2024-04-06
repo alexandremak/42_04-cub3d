a5e4f8c180dff2f1435b8b8e54ad6c3eddbe054d
Alexandre Mak <amak@c2r1s4.42lisboa.com>
2024-04-06 18:54:49 +0100

    raycast ok - merge with main to check all

--- src/raycast.c
+++ src/raycast.c
@@ -6,7 +6,7 @@
 /*   By: amak <amak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 18:17:32 by amak              #+#    #+#             */
-/*   Updated: 2024/04/06 15:11:46 by amak             ###   ########.fr       */
+/*   Updated: 2024/04/06 18:53:05 by amak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ static int		offsety(int y, float angle)
 		i--;
 		res = y - (i * PX) + 1;
 	}
-	printf("horizontal= %d  distancia= %d\n", i * PX, res);
+	// printf("horizontal= %d  distancia= %d\n", i * PX, res);
 	return (res);
 }
 
@@ -58,7 +58,7 @@ static int		offsetx(int x, float angle)
 			i++;
 		res = (i * PX) - x;
 	}
-	printf("vertical= %d distancia= %d\n", i * PX, res);
+	// printf("vertical= %d distancia= %d\n", i * PX, res);
 	return (res);
 }
 
@@ -73,7 +73,7 @@ static int calc_xdist(int x, int adj, float angle)
 	int_angle = (int)(angle * 1000000) - 1;
 	ninety = (int)(((PI / 2)) * 1000000);
 	twoseventy = (int)(((3 * PI) / 2) * 1000000);
-	printf("xdist: angle*1000000=%d | 90=%d | 270=%d\n", int_angle, ninety, twoseventy);
+	// printf("xdist: angle*1000000=%d | 90=%d | 270=%d\n", int_angle, ninety, twoseventy);
 	if (int_angle == ninety || int_angle == twoseventy)
 		return (0);
 	result = fabs(adj / cos(angle));
@@ -89,7 +89,7 @@ static int calc_ydist(int y, int opose, float angle)
 	result = 0;
 	int_angle = (int)(angle * 1000000);
 	oneeighty = (int)(PI * 1000000) + 1;
-	printf("ydist: angle*1000000=%d | 180=%d\n", int_angle, oneeighty);
+	// printf("ydist: angle*1000000=%d | 180=%d\n", int_angle, oneeighty);
 	if (int_angle == oneeighty || int_angle == 0)
 		return (0);
 	result = fabs(opose / sin(angle));
@@ -119,48 +119,51 @@ void	castray(t_image *image, t_player *player, t_file *file, float angle)
 	ra = angle;
 	dx = cos(angle);
 	dy = sin(angle);
+	i = 0;
 	while(!stop)
 	{
-		printf("%d TURN : ---------------\n", stop);
-		printf("ponto do raio: y= %d | x= %d\n", ry, rx);
-		printf("vector direccao: y= %f | x= %f\n", dy, dx);
-		printf("angulo direccao: %f\n", ra);
+		// printf("%d TURN : ---------------\n", stop);
+		// printf("ponto do raio: y= %d | x= %d\n", ry, rx);
+		// printf("vector direccao: y= %f | x= %f\n", dy, dx);
+		// printf("angulo direccao: %f\n", ra);
 		oy = offsety(ry, angle);
 		ox = offsetx(rx, angle);
-		printf("Y offset: %d\n", oy);
-		printf("X offset: %d\n", ox);
+		// printf("Y offset: %d\n", oy);
+		// printf("X offset: %d\n", ox);
 		disty = calc_ydist(ry, oy, ra);
 		distx = calc_xdist(rx, ox, ra);
-		printf("Y distance: %d\n", disty);
-		printf("X distance: %d\n", distx);
-		if (distx == 0 || disty <= distx)
+		// printf("Y distance: %d\n", disty);
+		// printf("X distance: %d\n", distx);
+		if (distx == 0 || (disty && disty <= distx))
 		{
 			if (dy < 0)
 				ry -= oy;
 			else
 				ry += oy;
-			printf("sin(ra)= %f\n", sin(ra));
+			// printf("sin(ra)= %f\n", sin(ra));
 			rx += disty * dx;
 			total += disty;
-			printf("Novo ponto do raio: y= %d | x= %d\n", ry, rx);
+			// printf("Novo ponto do raio: y= %d | x= %d\n", ry, rx);
 		}
-		else if (disty == 0 || disty > distx)
+		else if (disty == 0 || (distx && disty > distx))
 		{
 			if (dx < 0)
 				rx -= ox;
 			else
 				rx += ox;
-			printf("cos(ra)= %f\n", cos(ra));
+			// printf("cos(ra)= %f\n", cos(ra));
 			ry += distx * dy;
 			total += distx;
-			printf("Novo ponto do raio: y= %d | x= %d\n", ry, rx);
+			// printf("Novo ponto do raio: y= %d | x= %d\n", ry, rx);
 		}
-		printf("Novo ponto no mapa: y= %d | x= %d\n", ry / PX, rx / PX);
-		printf("Total: %f\n", total);
+		// printf("Novo ponto no mapa: y= %d | x= %d\n", ry / PX, rx / PX);
+		// printf("Total: %f\n", total);
 		if (check_wall(ry, rx, file->map))
 			stop = 1;
-		printf("--------------------------\n");
+		// printf("--------------------------\n");
+		i++;
 	}
+	printf("DDA count: %d\n", i);
 	i = 1;
 	while (i <= (int)total)
 	{
